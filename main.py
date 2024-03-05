d9fb3f45803cd4a2ea902b0f4f4a394bca7ae238
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-05 16:44:06 +0530

    Setting up the screen

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")                # Set background color
+screen.title("Snake Game")             # Set title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()         # Close window on click
