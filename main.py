b1c096dde614f8a7ddea3a9beb08628a7bb12a62
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-05 17:40:22 +0530

    Import snake file and create an object of snake class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,31 +9,15 @@ screen.title("Snake Game")             # Set title
 
 screen.tracer(0)                       # Turn off animation
 
-# Create 3 snake segments
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-turtle_segments = []
-
-for position in starting_positions:
-    tom = Turtle(shape="square")
-    tom.color("white")
-    tom.speed('normal')
-    tom.penup()
-    tom.setpos(position)
-    turtle_segments.append(tom)
-
+snake = Snake()
 
 game_is_on = True
+
 while game_is_on:
     screen.update()                  # Update the screen
     time.sleep(0.1)                  # Add delay
     
-    for seg_num in range(len(turtle_segments) - 1, 0, -1):
-        new_x = turtle_segments[seg_num-1].xcor()
-        new_y = turtle_segments[seg_num-1].ycor()
-        turtle_segments[seg_num].goto(new_x, new_y)
-        
-    turtle_segments[0].forward(20)     # Move the first segment forward
+    snake.move()                     # Move the snake
 
 
 
